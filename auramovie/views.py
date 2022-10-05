61107a306cf493a223e4597303315e87e5d503fe
Vipul <vborikar@vborikar-virtualbox.com>
2022-10-05 22:49:27 +0530

    * Working version with wait call

--- auramovie/views.py
+++ auramovie/views.py
@@ -16,16 +16,15 @@ headers = {
 	"X-RapidAPI-Host": "movie-database-alternative.p.rapidapi.com"
 }
 
+filename = ''
+
 def index(request):
-    #template = loader.get_template('index.html')
-    #return HttpResponse(template.render())
     return render(request, "index.html")
 
 def practise(request):
     return render(request, "practise.html")
 
-async def get_movie_info(filename_path):
-    '''
+def get_movie_info(filename_path):
     movies_dict = {}
     movies_dict['totalruntime'] = 0
     movies_dict['totalmovies'] = 0
@@ -60,37 +59,36 @@ async def get_movie_info(filename_path):
     movies_dict['totalruntime_hours'] = int(totalruntime/60)
     movies_dict['totalruntime_days'] = int(movies_dict['totalruntime_hours']/24)
     movies_dict['totalmovies'] = len(movies_dict['lists'])
-    #return movies_dict
-    '''
-    print("Inside get_movie_info")
-    await asyncio.sleep(10)
-    print("After Sleep")
-    return redirect('/practise')
-    return HttpResponse("Finished Loading")
+    return movies_dict
 
-async def get_csv_file(request):
+def get_csv_file(request):
+    #fs = FileSystemStorage()
+    filename_path = request.session.get('0')
+    print(filename_path)
+    #filename_path = fs.path(filename)
+    movies_dict = get_movie_info(filename_path)
+    movies_name_poster = zip(movies_dict['lists'],movies_dict["posters"])
+    #context = {'movies': movies_dict, 'img_path': img_path, 
+    #'movies_name_poster':movies_name_poster}
+    context = {'movies': movies_dict, 'movies_name_poster':movies_name_poster}
+    return render(request, "list_movies.html", context)
+
+def wait_page(request):
     if request.method == 'POST' and request.FILES['csvfile']:
-       
         myfile = request.FILES['csvfile']
         fs = FileSystemStorage()
         filename = fs.save(myfile.name, myfile)
         filename_path = fs.path(filename)
-        #movies_dict = get_movie_info(filename_path)
-        loop = asyncio.get_event_loop()
-        loop.create_task(get_movie_info(filename_path))
-        '''
-        img_path = fs.path("movie_1.jpg")
-        print(img_path)
-        movies_name_poster = zip(movies_dict['lists'],movies_dict["posters"])
-        context = {'movies': movies_dict, 'img_path': img_path, 
-        'movies_name_poster':movies_name_poster}
-        #return render(request, "list_movies.html", context)
-        '''
-        return HttpResponse("Waiting")
-    return HttpResponse("Waiting")
-
-def wait_page(request):
+        print(filename_path)
+        request.session['0'] = filename_path
     return render(request, "wait.html")
 
 
-  
+'''
+  <script>
+          $(document).ready(function(){
+              $('#movie_list').load("/auramovie/movielist");
+              $('#wait').hide()
+          });
+          </script>
+'''
